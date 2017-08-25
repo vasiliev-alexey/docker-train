d34e383a8ecbf91cfd3054450c291bef0130a07d
vasiliev.alexey <alex_v81@mail.ru>
2017-08-25 13:52:46 +0300

    Docker compose

---
+++ ch9/run_compose_on_do.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+echo "Для нормальной работы необходим docker-compose  не ниже версии 1.15"
+cd identidock/
+docker-compose  up -d
--- ch9/run_in_do.sh
+++ ch9/run_in_do.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 . ./token.env
-docker-machine create --driver digitalocean --digitalocean-access-token $DITOCKEN identihost-do 
+docker-machine create --driver digitalocean --digitalocean-access-token $DITOCKEN identihost-do
+
+eval "$(docker-machine env identihost-do)"
